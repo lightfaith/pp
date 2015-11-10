be79b4e18e022a1f3933f3974a026b17f2085049
Vítězslav Grygar <lightfaith@users.noreply.github.com>
2015-11-10 16:00:35 +0100

    More obvious version of PI

--- mpi/pi.cpp
+++ mpi/pi.cpp
@@ -12,11 +12,19 @@ int main(int argc, char *argv[])
 	}
 	int numtasks, rank, rc, ierr;
 	unsigned int intervals = atoi(argv[1]);
-	long int istart, iend, r;
-	double x, dx, f, sum, total_sum, pi;
+	long int start; //start of interval
+	long int  end; //end of interval
+	long int r;
+	double x; // x for formula
+	double intervalsize; 
+	double rectik; //size of one little rectangle
+	double thread_sum; //sum of rectangles for one thread
+	double total_sum; //total sum of all rectangles
+	double pi; //final pi (total_sum * intervalsize)
 	double time2, time1;
 	char realpi[] = {"3.14159265358979323846"};
 	char computedpi[23];
+	
 	rc = MPI_Init(&argc,&argv);
 	if (rc != MPI_SUCCESS)
 	{
@@ -34,22 +42,20 @@ int main(int argc, char *argv[])
 	}
 	
 	time1 = MPI_Wtime();
-	istart = (intervals/numtasks)*rank+1;
-	iend = (intervals/numtasks)*(rank+1);
-	sum=0.0;
+	start = (intervals/numtasks)*rank+1;
+	end = (intervals/numtasks)*(rank+1);
+	thread_sum=0.0;
 	total_sum=0.0;
-	dx = 1.0/(double)intervals;
-	for(int i = iend; i>=istart; i--)
-		x=dx*((double)(i-0.5));
-	for(int i=iend; i>=istart; i--)
+	intervalsize = 1.0/(double)intervals;
+	for(int i=end; i>=start; i--)
 	{
-		x = dx*((double)(i-0.5));
-		f = 4.0/ (1.0+x*x);
-		sum=sum+f;
+		x = intervalsize*((double)(i-0.5));
+		rectik = 4.0/ (1.0+x*x);
+		thread_sum=thread_sum+rectik;
 	}
-	ierr=MPI_Reduce(&sum, &total_sum, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+	ierr=MPI_Reduce(&thread_sum, &total_sum, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
 	time2 = MPI_Wtime();
-	pi=dx*total_sum;
+	pi=intervalsize*total_sum;
 	
 	if(rank==0)
 	{
@@ -61,7 +67,7 @@ int main(int argc, char *argv[])
 				bugloc++;
 			else
 				break;
-		printf("PI computed in %f:\n%.24f\n", time2-time1, pi);
+		printf("PI computed in %f:\n%.24fs\n", time2-time1, pi);
 		if(bugloc<23)
 			for(int i=1; i<bugloc; i++)
 				printf(" ");
